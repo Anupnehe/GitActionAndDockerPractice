59647628240f7694b3bd822c57f75960776b6841
Anupnehe <106077051+Anupnehe@users.noreply.github.com>
2025-11-15 14:07:33 +0530

    added dockerfile

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -1,19 +1,20 @@
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
-public class ActionClass {
-    public static void main (String [] args){
+public class login {
 
+    @Test
+    public static void RunTest(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://ultimateqa.com/filling-out-forms/");
 
 
-      WebElement ele = driver.findElement(By.id("et_pb_contact_message_0"));
+        WebElement ele = driver.findElement(By.id("et_pb_contact_message_0"));
         ele.sendKeys("hello");
 
 
@@ -23,9 +24,9 @@ public class ActionClass {
 
         actions.sendKeys(Keys.ENTER).perform();
         actions.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).perform();
+
+        driver.quit();
+        System.out.println("completed");
+
     }
-
-
-
-
 }
